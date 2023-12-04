43ed9c97e695c7f382b46ca529af586cb27acc38
Kenji Dasal <35777115+KenjiDasal@users.noreply.github.com>
2023-12-04 12:59:18 +0000

    server updated

--- CA-GooglePlayStore-main/server.js
+++ CA-GooglePlayStore-main/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { JsonWebTokenError } = require('jsonwebtoken');
 const app = express();
-const port = 3000;
+const port = 3200;
 require('dotenv').config();
 require('./config/db.js')();
 const jwt = require('jsonwebtoken');
