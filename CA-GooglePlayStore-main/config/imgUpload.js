289eb76373dab5791f43dee52df1cba99689950f
Kenji Dasal <35777115+KenjiDasal@users.noreply.github.com>
2023-12-04 12:58:18 +0000

    S3 Uploaded

--- CA-GooglePlayStore-main/config/imgUpload.js
+++ CA-GooglePlayStore-main/config/imgUpload.js
@@ -1,5 +1,53 @@
 const multer = require('multer');
 const path = require('path');
+const { S3Client } = require('@aws-sdk/client-s3')
+const multerS3 = require('multer-s3');
+
+let storage;
+
+
+if(process.env.STORAGE_ENGINE === 'S3'){
+
+    const s3 = new S3Client({
+
+        region: process.env.MY_AWS_REGION,
+        credentials: {
+
+            accessKeyId: process.env.MY_AWS_KEY_ID,
+            secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY
+        }
+
+    });
+
+    storage = multerS3({
+
+        s3: s3,
+        bucket: process.env.MY_AWS_BUCKET,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: function(req, file, cb) {
+
+            cb(null, {fieldName: file.fieldname});
+
+        },
+        key: function(req, file, cb) {
+            cb(null, Date.now() + path.extname(file.originalname));
+        }
+
+    });
+
+}
+else{
+
+    storage = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, 'public/uploads');
+        },
+        filename: function(req, file, cb) {
+            cb(null, Date.now() + path.extname(file.originalname));
+        }
+    });
+
+}
 
 const fileFilter = (req, file, cb) => {
     if(!file){
@@ -15,13 +63,5 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'public/uploads');
-    },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
 
 module.exports = multer({ fileFilter, storage });
--- CA-GooglePlayStore-main/controllers/app.controller.js
+++ CA-GooglePlayStore-main/controllers/app.controller.js
@@ -1,182 +1,249 @@
-const App = require('../models/app.model');
 const fs = require('fs');
+const App = require('../models/app.model');
+
+
+const deleteImage = async (filename) => {
+    
+    if(process.env.STORAGE_ENGINE === 'S3'){
+
+        const { S3Client, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+        const s3 = new S3Client({
+
+            region: process.env.MY_AWS_REGION,
+            credentials: {
+    
+                accessKeyId: process.env.MY_AWS_KEY_ID,
+                secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY
+            }
+    
+        });
+
+        try{
+
+            const data = await s3.send(new DeleteObjectCommand({Bucket: process.env.MY_AWS_BUCKET, Key: filename}));
+            console.log("Successfully. deleted.", data);
+
+        }
+        catch{
+
+            console.error(err);
+
+        }
+
+    }else {
+
+        let path = `public/uploads/${filename}`;
+
+        fs.access(path, fs.constants.F_OK, (err) => {
+            if(err) {
+                console.error(err);
+                return;
+            }
+
+            fs.unlink(path, (err) => {
+                if(err) throw err;
+                console.log(`${filename} was deleted!`);
+            });
+        });
+
+    }
+
+};
+
 
 const readData = (req, res) => {
-  App.find()
-      .then((data) => {
-          console.log(data);
-          if(data.length > 0){
-              res.status(200).json(data);
-          }
-          else{
-              res.status(404).json("None found");
-          }
-      })
-      .catch((err) => {
-          console.log(err);
-          // res.status(500).json(err);
-      });
+    App.find()
+        .then((data) => {
+            console.log(data);
+            if(data.length > 0){
+                res.status(200).json(data);
+            }
+            else{
+                res.status(404).json("None found");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 };
 
 const readOne = (req, res) => {
 
-  let id = req.params.id;
+    let id = req.params.id;
 
-  App.findById(id)
-      .then((data) => {
+    App.findById(id)
+        .then((data) => {
 
-          if(data){
-              res.status(200).json(data);
-          }
-          else {
-              res.status(404).json({
-                  "message": `App with id: ${id} not found`
-              });
-          }
-          
-      })
-      .catch((err) => {
-          console.error(err);
-          if(err.name === 'CastError') {
-              res.status(400).json({
-                  "message": `Bad request, ${id} is not a valid id`
-              });
-          }
-          else {
-              res.status(500).json(err)
-          }
-          
-      });
+            if(data){
+                    // let img = `${process.env.STATIC_FIES_URL}${data.image_path}`;
+                    // data.image_path = img;
+                res.status(200).json(data);
+            }
+            else {
+                res.status(404).json({
+                    "message": `App with id: ${id} not found`
+                });
+            }
+            
+        })
+        .catch((err) => {
+            console.error(err);
+            if(err.name === 'CastError') {
+                res.status(400).json({
+                    "message": `Bad request, ${id} is not a valid id`
+                });
+            }
+            else {
+                res.status(500).json(err)
+            }
+            
+        });
 
 };
 
 const createData = (req, res) => {
-  console.log(req.file);
-  let appData = req.body;
-  if (req.file) {
-    appData.image_path = req.file.filename;
-    //include the following else if image is required
-  } else {
-    return res.status(422).json({
-      mgs: 'Image not uploaded',
-    });
-  }
-  App.create(req.body)
-    .then((data) => {
-      console.log('New App Created!', data);
-      res.status(201).json(data);
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        console.error('Validation Error!!', err);
-        res.status(422).json({
-          msg: 'Validation Error',
-          error: err.message,
+
+    console.log(req.file);
+
+    let appData = req.body;
+
+    if(req.file){
+        appData.image_path = process.env.STORAGE_ENGINE === 'S3' ? req.file.key : req.file.filename;
+    }
+    // include the following else if image is required
+    else {
+        return res.status(422).json({
+            msg: "Image not uploaded!!"
         });
-      } else {
-        console.error(err);
-        res.status(500).json(err);
-      }
-    });
+    }
+
+    App.create(appData)
+        .then((data) => {
+            console.log('New App Created!', data);
+            res.status(201).json(data);
+        })
+        .catch((err) => {
+            if(err.name === 'ValidationError'){
+                console.error('Validation Error!!', err);
+                res.status(422).json({
+                    "msg": "Validation Error",
+                    "error" : err.message 
+                });
+            }
+            else {
+                console.error(err);
+                res.status(500).json(err);
+            }
+        });
+
 };
 
 const updateData = (req, res) => {
-  let id = req.params.id;
-  let body = req.body;
 
-  if (req.file) {
-    body.image_path = req.file.filename;
-  }
-  //else {
-  //   return res.status(422).json({
-  //     mgs: 'Image not uploaded',
-  //   });
-  // }
-  App.findByIdAndUpdate(id, body, {
-    new: false,
-  })
-    .then((data) => {
-      deleteImg(data.image_path);
-      if (data) {
-        res.status(201).json(data);
-      } else {
-        deleteImg(body.data.image_path);
-        res.status(404).json({
-          message: `App with id: ${id} not found`,
-        });
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        console.error('Validation Error!!', err);
-        res.status(422).json({
-          msg: 'Validation Error',
-          error: err.message,
-        });
-      } else if (err.name === 'CastError') {
-        res.status(400).json({
-          message: `Bad request, ${id} is not a valid id`,
-        });
-      } else {
-        console.error(err);
-        res.status(500).json(err);
-      }
-    });
-};
+    let id = req.params.id;
+    let body = req.body;
 
-const deleteImg = (filename) => {
-  let path = `/public/uploads/${filename}`;
-
-  fs.access(path, fs.constants.F_OK, (err) => {
-    if (err) {
-      console.log(err);
-      return;
+    if(req.file){
+        body.image_path = req.file.filename
     }
-    fs.unlink(path, (err) => {
-      if (err) throw err;
-      console.log(`${filename} was deleted`);
-    });
-  });
+    // include the following else if image is required
+    // else {
+    //     return res.status(422).json({
+    //         msg: "Image not uploaded!!"
+    //     });
+    // }
+
+    App.findByIdAndUpdate(id, body, {
+        new: false
+    })
+        .then((data) => {
+
+            console.log(data);
+
+            if(data){
+
+                deleteImage(data.image_path);
+                res.status(201).json(data);
+            }
+            else {
+                deleteImage(body.image_path);
+                res.status(404).json({
+                    "message": `App with id: ${id} not found`
+                });
+            }
+            
+        })
+        .catch((err) => {
+            if(err.name === 'ValidationError'){
+                console.error('Validation Error!!', err);
+                res.status(422).json({
+                    "msg": "Validation Error",
+                    "error" : err.message 
+                });
+            }
+            else if(err.name === 'CastError') {
+                res.status(400).json({
+                    "message": `Bad request, ${id} is not a valid id`
+                });
+            }
+            else {
+                console.error(err);
+                res.status(500).json(err);
+            }
+        });
+
+
 };
 
 const deleteData = (req, res) => {
-  let id = req.params.id;
-  let imgPath = '';
 
-  App.findById({ _id: id })
-    .then((data) => {
-      console.log('data', data);
-      if (data) {
-        imgPath = data.image_path;
-        return data.deleteOne();
-      } else {
-        res.status(404).json({
-          msg: 'Could not find App',
+    let id = req.params.id;
+    let imagePath = '';
+
+    App.findById(id)
+        .then((data) => {
+
+            console.log('data', data);
+
+            if(data){
+                imagePath = data.image_path;
+                return data.deleteOne();
+            }
+            else {
+                res.status(404).json({
+                    "message": `App with id: ${id} not found`
+                });
+            }
+        })
+        .then(() => {
+            deleteImage(imagePath);
+
+            res.status(200).json({
+                "message": `App with id: ${id} deleted successfully`
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            if(err.name === 'CastError') {
+                res.status(400).json({
+                    "message": `Bad request, ${id} is not a valid id`
+                });
+            }
+            else {
+                res.status(500).json(err)
+            } 
         });
-      }
-    })
-    .then(() => {
-      deleteImg(imgPath);
-      res.status(200).json({
-        msg: 'App was deleted',
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.name === 'CastError') {
-        res.status(400).json({
-          message: `Bad request, ${id} is not a valid id`,
-        });
-      } else {
-        res.status(500).json(err);
-      }
-    });
+
+
 };
 
 module.exports = {
-  readData,
-  readOne,
-  createData,
-  updateData,
-  deleteData,
+    readData,
+    readOne,
+    createData,
+    updateData,
+    deleteData
 };
+
+
