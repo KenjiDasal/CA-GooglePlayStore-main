7be68d2bd4ad65114a3b3daabbfbf42024d941f6
Kenji Dasal <35777115+KenjiDasal@users.noreply.github.com>
2023-12-06 13:24:44 +0000

    Testing

--- models/user.model.js
+++ models/user.model.js
@@ -1,29 +1,31 @@
-const { Schema, model, trusted } = require('mongoose');
+const { Schema, model } = require('mongoose');
 const bcrypt = require('bcryptjs');
-const userSchema = new Schema(
-  {
+
+// The model takes in the types of data that are specified, some are required and some are optional
+const userSchema = new Schema({
     full_name: {
-      type: String,
-      trim: true,
-      required: true,
+        type: String,
+        trim: true,
+        required: true
     },
     email: {
-      type: String,
-      trim: true,
-      unique: true,
-      required: true,
-      lowercase: true,
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+        lowercase: true
     },
     password: {
-      type: String,
-      required: true,
-      min: 6,
-    },
-  },
-  { timestamp: true }
-);
+        type: String,
+        required: true,
+        min: 6
+    }
+}, { timestamps: true });
 
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compareSync(password, this.password);
+// uses bcrypt to compare passwords
+userSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
 };
+
+// exporting the model
 module.exports = model('User', userSchema);
